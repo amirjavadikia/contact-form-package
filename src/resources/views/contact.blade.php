016b6d064f34936a2fc871404981914da753470a
amirjavadikia <amir.m.javadikia@gmail.com>
2022-12-31 11:47:08 +0330

    change some commits

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -9,14 +9,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1 class="text-center">contact us page</h1>
+    <h1>contact us page</h1>
 
     <form action="{{ route('contact') }}"  class="my-3 container"  method="post">
         @csrf
-        <input type="text" class="form-control my-3" name="name" placeholder="your name">
-        <input type="email" class="form-control my-3" name="email" placeholder="your email">
-        <textarea name="message"  class="form-control my-3 "  cols="30" rows="10" placeholder="your message"></textarea>
-        <input type="submit" class="btn btn-success my-3" value="submit">
+        <input type="text" class="form-control" name="name" placeholder="your name">
+        <input type="email" class="form-control" name="email" placeholder="your email">
+        <textarea name="message"  class="form-control"  cols="30" rows="10" placeholder="your message"></textarea>
+        <input type="submit" class="btn btn-success" value="submit">
     </form>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
